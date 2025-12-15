5c18e0fa93ecf266f1b1b1d0408b70163e8ddfb8
Alec <alex.laing@me.com>
2025-12-15 19:13:45 +1300

    Added Thing inspector

--- shards/playground/playground.gd
+++ shards/playground/playground.gd
@@ -4,5 +4,6 @@ func _ready() -> void:
 	super()
 	$Landscape/Col.set_collision_layer_value(2, true)
 	
-	await get_tree().create_timer(2.0).timeout
-	$Sound/Music.play()
+	# TODO: proper jukebox
+	#await get_tree().create_timer(2.0).timeout
+	#$Sound/Music.play()
