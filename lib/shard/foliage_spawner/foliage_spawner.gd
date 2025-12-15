daa21a4e3d9eafd7d6ab350b090dbf0e6907e93a
Alex Laing <alex.laing@me.com>
2025-12-15 16:28:16 +1300

    Removed vertices from scaffolding

--- lib/shard/foliage_spawner/foliage_spawner.gd
+++ lib/shard/foliage_spawner/foliage_spawner.gd
@@ -71,3 +71,6 @@ func _ready() -> void:
 	cast_shadow = SHADOW_CASTING_SETTING_OFF
 	set_layer_mask_value(1, 0)
 	set_layer_mask_value(2, 1)
+	
+	if !Engine.is_editor_hint():
+		$Selector.queue_free()
