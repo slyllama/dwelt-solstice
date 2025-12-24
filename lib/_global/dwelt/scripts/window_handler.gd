156ec60ebcfb084a6834d01a68868fdf02d43373
Alec <alex.laing@me.com>
2025-12-24 23:40:19 +1300

    Improved macOS display

--- lib/_global/dwelt/scripts/window_handler.gd
+++ lib/_global/dwelt/scripts/window_handler.gd
@@ -18,9 +18,12 @@ func _ready() -> void:
 			)
 	
 	if _is_retina():
-		get_window().content_scale_factor = 2.0
-		# TODO: don't do this on macOS (it automatically handles cursor scaling)
-		DisplayServer.cursor_set_custom_image(load("res://generic/textures/cursor_2x.png"))
+		if OS.get_name() == "macOS":
+			get_window().content_scale_factor = 2.25
+		else:
+			get_window().content_scale_factor = 2.0
+			DisplayServer.cursor_set_custom_image(
+				load("res://generic/textures/cursor_2x.png"))
 		Dwelt.window_adjusted.emit()
 	
 	if Engine.is_embedded_in_editor(): return
